807918ef8134bb5bd6d3afd394b54e5f6066d899
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-02 16:47:49 +0200

    Tache 2 Créer la classe Oviparous / 2.1 Ajouter l'attribut incubationPeriod / 2.2 Créer la méthode layEgg()

--- src/main/java/fr/g4zoo/fantastizoo/models/creatures/Oviparous.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/creatures/Oviparous.java
@@ -1,4 +1,20 @@
 package fr.g4zoo.fantastizoo.models.creatures;
 
-public class Oviparous extends Creature{
+public abstract class Oviparous extends Creature {
+
+    private int incubationPeriod;
+
+    // GETTERS
+
+    public int getIncubationPeriod() {
+        return incubationPeriod;
+    }
+
+    // SETTERS
+
+    public void setIncubationPeriod(int incubationPeriod) {
+        this.incubationPeriod = incubationPeriod;
+    }
+
+    public abstract void layEgg();
 }
