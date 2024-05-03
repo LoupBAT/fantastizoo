b8a8503e743c894b6342a9b60da037ffd5626912
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-03 10:53:58 +0200

    Ajout nombre d'action ZooMaster

--- src/main/java/fr/g4zoo/fantastizoo/models/ZooMaster.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/ZooMaster.java
@@ -6,36 +6,49 @@ public class ZooMaster {
     private Character gender;
     private Integer age;
     private Integer hp;
+    private Integer action;
+
+    //GETTERS
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Character getGender() {
         return gender;
     }
 
-    public void setGender(Character gender) {
-        this.gender = gender;
-    }
-
     public Integer getAge() {
         return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
     }
 
     public Integer getHp() {
         return hp;
     }
 
+    public Integer getAction() {
+        return action;
+    }
+
+    //SETTERS
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     public void setHp(Integer hp) {
         this.hp = hp;
+    }
+
+    public void setAction(Integer action) {
+        this.action = action;
     }
 }
