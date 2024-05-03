b4e89d685168b17d7874968d895abc3a73f4a357
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-03 10:15:55 +0200

    Tache 19 Créer la classe ZooMaster / 19.1 Créer les attributs de ZooMaster

--- src/main/java/fr/g4zoo/fantastizoo/models/ZooMaster.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/ZooMaster.java
@@ -1,4 +1,41 @@
 package fr.g4zoo.fantastizoo.models;
 
 public class ZooMaster {
+
+    private String name;
+    private Character gender;
+    private Integer age;
+    private Integer hp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
 }
