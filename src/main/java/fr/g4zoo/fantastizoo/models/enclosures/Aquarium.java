a6e159e5623d094cda756104497a5559cc69babc
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-18 14:33:47 +0200

    18. Créer la classe Aquarium / 18.1 Ajouter les attributs de Aquarium / 18.2 Réécrire la méthode getCleaned()

--- src/main/java/fr/g4zoo/fantastizoo/models/enclosures/Aquarium.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/enclosures/Aquarium.java
@@ -1,4 +1,44 @@
 package fr.g4zoo.fantastizoo.models.enclosures;
 
-public class Aquarium extends Enclosure{
+public class Aquarium extends Enclosure {
+    private double depth;
+    private double waterSalinity;
+
+    // Constructor
+    public Aquarium(String name, double area, double depth, double waterSalinity) {
+        super(name, area);
+        this.depth = depth;
+        this.waterSalinity = waterSalinity;
+    }
+
+    // Getters et Setters pour depth et waterSalinity
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public double getWaterSalinity() {
+        return waterSalinity;
+    }
+
+    public void setWaterSalinity(double waterSalinity) {
+        this.waterSalinity = waterSalinity;
+    }
+
+    // Overide to include the depth and waterSalinity
+    @Override
+    public void displayInformations() {
+        super.displayInformations();
+        System.out.println("Profondeur : " + depth + " m");
+        System.out.println("Salinité de l'eau : " + waterSalinity + " ‰");
+    }
+
+    @Override
+    public void getCleaned() {
+        super.getCleaned();
+        System.out.println("La vérification de la profondeur du bassin et la salinité de l'eau a été faite.");
+    }
 }
