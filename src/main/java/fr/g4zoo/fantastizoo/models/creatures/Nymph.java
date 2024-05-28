cc3a2c0c51fd575ea296f2df8fb39d2f00813597
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-28 17:13:53 +0200

    12.Créer la classe Nymph / 12.2 Intégrer la méthode giveBirth() / 12.3Intégrer les méthodes de Creature

--- src/main/java/fr/g4zoo/fantastizoo/models/creatures/Nymph.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/creatures/Nymph.java
@@ -1,11 +1,53 @@
 package fr.g4zoo.fantastizoo.models.creatures;
 
 import fr.g4zoo.fantastizoo.models.creatures.interfaces.Reborner;
+import fr.g4zoo.fantastizoo.models.enclosures.Enclosure;
 
 public class Nymph extends Viviparous implements Reborner {
+    private static final int DEFAULT_GESTATION_PERIOD = 6;
+    private static final String[] NAMES = {"Astra", "Luna", "Selene", "Nyx", "Aurora", "Celeste", "Eos", "Nova"};
+    private Enclosure enclosure;
+
+    // Constructor
+    public Nymph(String name, Enclosure enclosure, char gender, int age, double weight, double height) {
+        this.setName(name);
+        this.setGestationLength(DEFAULT_GESTATION_PERIOD);
+        this.enclosure = enclosure;
+        this.setGender(gender);
+        this.setAge(age);
+        this.setWeight(weight);
+        this.setHeight(height);
+        enclosure.addCreature(this);
+    }
 
     @Override
     public void giveBirth() {
+        if (this.getGender() != 'f' || this.getAge() < 18) {
+            System.out.println(this.getName() + " ne peut pas donner naissance.");
+            return;
+        }
 
+        System.out.println(this.getName() + " est enceinte.");
+        simulateGestationPeriod(() -> {
+            Nymph babyNymph = createBabyNymph();
+            System.out.println(this.getName() + " a donné naissance à une bébé nymphe : " + babyNymph.getName() + " !");
+            addBabyNymphToZoo(babyNymph);
+        });
+    }
+
+    private Nymph createBabyNymph() {
+        String babyName = generateRandomName(NAMES);
+        char gender = generateRandomGender();
+        double weight = generateRandomWeight(2.0, 4.0);
+        double height = generateRandomHeight(0.5, 1.0);
+
+        return new Nymph(babyName, this.enclosure, gender, 1, weight, height);
+    }
+
+    private void addBabyNymphToZoo(Nymph babyNymph) {
+        if (this.enclosure != null) {
+            this.enclosure.addCreature(babyNymph);
+        }
+        System.out.println("Bébé nymphe ajouté au zoo : " + babyNymph.getName());
     }
 }
