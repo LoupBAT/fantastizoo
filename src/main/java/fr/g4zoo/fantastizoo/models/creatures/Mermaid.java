7bac2a55d43fc3b2454014dd587479136bc4cbf4
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-31 12:44:17 +0200

    14. Créer la classe Unicorn / 14.1 Implémentez l'interface Runner / 14.2 Intégrer la méthode giveBirth() / 14.3 Intégrer les méthodes de Creature

--- src/main/java/fr/g4zoo/fantastizoo/models/creatures/Mermaid.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/creatures/Mermaid.java
@@ -1,15 +1,58 @@
 package fr.g4zoo.fantastizoo.models.creatures;
 
 import fr.g4zoo.fantastizoo.models.creatures.interfaces.Swimmer;
+import fr.g4zoo.fantastizoo.models.enclosures.Enclosure;
 
 public class Mermaid extends Viviparous implements Swimmer {
+    private static final int DEFAULT_GESTATION_PERIOD = 12;
+    private static final String[] NAMES = {"Ariel", "Marina", "Nerissa", "Ondine", "Sirena", "Thalassa"};
+    private Enclosure enclosure;
+
+    // Constructor
+    public Mermaid(String name, Enclosure enclosure, char gender, int age, double weight, double height) {
+        this.setName(name);
+        this.setGestationLength(DEFAULT_GESTATION_PERIOD);
+        this.enclosure = enclosure;
+        this.setGender(gender);
+        this.setAge(age);
+        this.setWeight(weight);
+        this.setHeight(height);
+        enclosure.addCreature(this);
+    }
 
     @Override
     public void giveBirth() {
+        if (this.getGender() != 'f' || this.getAge() < 18) {
+            System.out.println(this.getName() + " ne peut pas donner naissance.");
+            return;
+        }
 
+        System.out.println(this.getName() + " est enceinte.");
+        simulateGestationPeriod(() -> {
+            Mermaid babyMermaid = createBabyMermaid();
+            System.out.println(this.getName() + " a donné naissance à une bébé sirène : " + babyMermaid.getName() + " !");
+            addBabyMermaidToZoo(babyMermaid);
+        });
     }
 
-    public void swim() {
+    private Mermaid createBabyMermaid() {
+        String babyName = generateRandomName(NAMES);
+        char gender = generateRandomGender();
+        double weight = generateRandomWeight(2.0, 4.0);
+        double height = generateRandomHeight(0.5, 1.0);
 
+        return new Mermaid(babyName, this.enclosure, gender, 1, weight, height);
+    }
+
+    private void addBabyMermaidToZoo(Mermaid babyMermaid) {
+        if (this.enclosure != null) {
+            this.enclosure.addCreature(babyMermaid);
+        }
+        System.out.println("Bébé sirène ajouté au zoo : " + babyMermaid.getName());
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(this.getName() + " nage avec élégance !");
     }
 }
