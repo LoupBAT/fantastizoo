4878e92816ca2bcd5d7f346d14453f0836a3e5b8
Fabien Pinel <fabien.pinel08@gmail.com>
2024-05-31 12:48:49 +0200

    15. Créer la classe Mermaid /  15.1 Implémentez l'interface Swimmer / 15.2 Intégrer la méthode giveBirth() / 15.3 Intégrer les méthodes de Creature

--- src/main/java/fr/g4zoo/fantastizoo/models/creatures/Unicorn.java
+++ src/main/java/fr/g4zoo/fantastizoo/models/creatures/Unicorn.java
@@ -1,15 +1,58 @@
 package fr.g4zoo.fantastizoo.models.creatures;
 
 import fr.g4zoo.fantastizoo.models.creatures.interfaces.Runner;
+import fr.g4zoo.fantastizoo.models.enclosures.Enclosure;
 
 public class Unicorn extends Viviparous implements Runner {
+    private static final int DEFAULT_GESTATION_PERIOD = 11;
+    private static final String[] NAMES = {"Celestia", "Sparkle", "Blaze", "Luna", "Aurora", "Starlight"};
+    private Enclosure enclosure;
+
+    // Constructor
+    public Unicorn(String name, Enclosure enclosure, char gender, int age, double weight, double height) {
+        this.setName(name);
+        this.setGestationLength(DEFAULT_GESTATION_PERIOD);
+        this.enclosure = enclosure;
+        this.setGender(gender);
+        this.setAge(age);
+        this.setWeight(weight);
+        this.setHeight(height);
+        enclosure.addCreature(this);
+    }
 
     @Override
     public void giveBirth() {
+        if (this.getGender() != 'f' || this.getAge() < 18) {
+            System.out.println(this.getName() + " ne peut pas donner naissance.");
+            return;
+        }
 
+        System.out.println(this.getName() + " est enceinte.");
+        simulateGestationPeriod(() -> {
+            Unicorn babyUnicorn = createBabyUnicorn();
+            System.out.println(this.getName() + " a donné naissance à un bébé licorne : " + babyUnicorn.getName() + " !");
+            addBabyUnicornToZoo(babyUnicorn);
+        });
     }
 
-    public void run() {
+    private Unicorn createBabyUnicorn() {
+        String babyName = generateRandomName(NAMES);
+        char gender = generateRandomGender();
+        double weight = generateRandomWeight(3.0, 5.0);
+        double height = generateRandomHeight(0.7, 1.2);
 
+        return new Unicorn(babyName, this.enclosure, gender, 1, weight, height);
+    }
+
+    private void addBabyUnicornToZoo(Unicorn babyUnicorn) {
+        if (this.enclosure != null) {
+            this.enclosure.addCreature(babyUnicorn);
+        }
+        System.out.println("Bébé licorne ajouté au zoo : " + babyUnicorn.getName());
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.getName() + " court avec grâce !");
     }
 }
